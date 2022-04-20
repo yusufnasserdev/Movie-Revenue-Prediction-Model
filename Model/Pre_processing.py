f73cd10fe468909897ddd6d685b55d8466409a7b
Steven Sameh Soliman <42946298+stevensameh@users.noreply.github.com>
2022-04-21 01:14:43 +0200

    Updated movie-revenue dataset and trained a basic model
    Co-Authored-By: MostafaAymanAwad <99618393+MostafaAymanAwad@users.noreply.github.com>
    Co-Authored-By: Youssef Nasser <yesyosife@gmail.com>

---
+++ Model/Pre_processing.py
@@ -0,0 +1,15 @@
+from sklearn.preprocessing import LabelEncoder
+import numpy as np
+def Feature_Encoder(X,cols):
+    for c in cols:
+        lbl = LabelEncoder()
+        lbl.fit(list(X[c].values))
+        X[c] = lbl.transform(list(X[c].values))
+    return X
+
+def featureScaling(X,a,b):
+    X = np.array(X)
+    Normalized_X=np.zeros((X.shape[0],X.shape[1]))
+    for i in range(X.shape[1]):
+        Normalized_X[:,i]=((X[:,i]-min(X[:,i]))/(max(X[:,i])-min(X[:,i])))*(b-a)+a
+    return Normalized_X
---
+++ Model/basicModel.py
@@ -0,0 +1,34 @@
+from sklearn import linear_model
+from sklearn import metrics
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import PolynomialFeatures
+import seaborn as sns
+from Pre_processing import *
+
+movies = pd.read_csv('datasets/movies-revenue.csv')
+X = movies
+Y = X['revenue']
+X.drop(['title', 'director'], axis=1, inplace=True)
+
+X = featureScaling(X, 0, 100)
+X = pd.DataFrame(X, columns=movies.keys())
+corr = X.corr()
+top_feature = corr.index[abs(corr['revenue']) > 0.2]
+plt.subplots(figsize=(12, 8))
+top_corr = X[top_feature].corr()
+sns.heatmap(top_corr, annot=True)
+plt.show()
+top_feature = top_feature.delete(top_feature.get_loc('revenue'))
+X = X[top_feature]
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, random_state=20, test_size=0.2, shuffle=True)
+
+poly_features = PolynomialFeatures(degree=3)
+X_train_poly = poly_features.fit_transform(X_train)
+poly_model = linear_model.LinearRegression()
+poly_model.fit(X_train_poly, Y_train)
+Y_test_predicted = poly_model.predict(poly_features.transform(X_test))
+prediction = poly_model.predict(poly_features.fit_transform(X_test))
+
+print('Mean Square Error', metrics.mean_squared_error(Y_test, Y_test_predicted))
