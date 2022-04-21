3adb4ac88108cd0c082d3ae380987d407be05ed8
Yusuf Nasser <yesyosife@gmail.com>
2022-04-21 23:45:56 +0200

    Refined milestone 1 report.
    Co-authored-by: MostafaAymanAwad <ayelshekh@gmail.com>
    Co-authored-by: stevensameh <stevensameh1512@gmail.com>

--- Model/regularization_model.py
+++ Model/regularization_model.py
@@ -16,11 +16,11 @@ def model_trial(x_train, x_test, y_traine, y_test, model, degree=30):
 
     train_err = metrics.mean_squared_error(y_train, y_train_predicted)
     test_err = metrics.mean_squared_error(y_test, prediction)
-    # print('Train subset (MSE) for degree {}: '.format(degree), train_err)
+    print('Train subset (MSE) for degree {}: '.format(degree), train_err)
     print('Test subset (MSE) for degree {}: '.format(degree), test_err)
 
 
-# Load players data
+# Load movies data
 data = pd.read_csv('datasets/movies-revenue.csv')
 # Drop the rows that contain missing values
 data.dropna(how='any', inplace=True)
