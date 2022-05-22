44710132c73eac5650e55eb2f704c3a54f8657af
Yusuf Nasser <yesyosife@gmail.com>
2022-05-22 09:11:24 +0200

    Cleaned up Phase 2 files
    Co-authored-by: MostafaAymanAwad <ayelshekh@gmail.com>
    Co-authored-by: stevensameh <stevensameh1512@gmail.com>

--- Model/Phase 2/Pre_processing.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-
-def featureScaling(X, a, b):
-    X = np.array(X)
-    Normalized_X = np.zeros((X.shape[0], X.shape[1]))
-    for i in range(X.shape[1]):
-        Normalized_X[:, i] = ((X[:, i] - min(X[:, i])) / (max(X[:, i]) - min(X[:, i]))) * (b - a) + a
-    return Normalized_X
--- Model/Phase 2/outliersRemoval.py
+++ Model/Phase 2/outliersRemoval.py
@@ -1,19 +1,21 @@
 import pandas as pd
 import warnings
+
 warnings.filterwarnings("ignore")
+
 
 def cap_data(df):
     for col in df.columns:
-        if (((df[col].dtype)=='float64') | ((df[col].dtype)=='int64')):
-            percentiles = df[col].quantile([0.25,0.75]).values
+        if (((df[col].dtype) == 'float64') | ((df[col].dtype) == 'int64')):
+            percentiles = df[col].quantile([0.25, 0.75]).values
             df[col][df[col] <= percentiles[0]] = percentiles[0]
             df[col][df[col] >= percentiles[1]] = percentiles[1]
         else:
-            df[col]=df[col]
+            df[col] = df[col]
     return df
 
 
 movies = pd.read_csv('datasets/movies-revenue-classification.csv')
 movies.drop(['movie_title', 'director'], axis=1, inplace=True)
 movies = cap_data(movies)
-movies.to_csv('out.csv')
+movies.to_csv('datasets/movies-revenue-classification.csv')
