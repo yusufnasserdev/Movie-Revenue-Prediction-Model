dab561b242c2c0dbd4297506d775eb745e59fe03
yusufnasserdev <yusufnassereng@gmail.com>
2023-01-03 07:45:40 +0200

    Modified phase1 notebook

--- Model/Phase 1/join.py
+++ Model/Phase 1/join.py
@@ -5,7 +5,7 @@ movies_base = pd.read_csv('datasets/movies-revenue.csv')
 movies_director = pd.read_csv('datasets/movie-director.csv')
 
 # using merge function by setting how='outer'
-output4 = pd.merge(movies_base, movies_director, on='title', how='left')
+output = pd.merge(movies_base, movies_director, on='title', how='left')
 
 # displaying result
-output4.to_csv('datasets/movies-revenue.csv')
+output.to_csv('datasets/movies-revenue.csv')
