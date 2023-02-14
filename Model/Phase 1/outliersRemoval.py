48339c4eca01d8ff1510c63c1e7a9e4fee0022b4
yusufnasserdev <yusufnassereng@gmail.com>
2023-02-14 19:20:37 +0200

    Deleted multiple phase1 .py files
    Deleted files that contained code which had already been incorporated in the notebook

--- Model/Phase 1/outliersRemoval.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-movies = pd.read_csv('datasets/movies-revenue.csv')
-q_low = movies.quantile(0.25)
-q_hi = movies.quantile(0.75)
-IQR = q_hi - q_low
-output = (movies < (q_low - 1.5 * IQR)) | (movies > (q_hi + 1.5 * IQR))
-output.to_csv('datasets/movies-revenue.csv')
--- Model/Phase 1/revenueTransform.py
+++
@@ -1,8 +0,0 @@
-from re import sub
-from decimal import Decimal
-import pandas as pd
-movies = pd.read_csv('datasets/movies-revenue.csv')
-for i, mov in movies.iterrows():
-    movies.at[i, 'revenue'] = Decimal(sub(r'[^\d.]', '', mov['revenue']))
-
-movies.to_csv('datasets/movies-revenue.csv')
