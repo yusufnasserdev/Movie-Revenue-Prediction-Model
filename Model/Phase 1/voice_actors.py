1604e6d7282d115ec7f0c1cd4607c9c1abed4a39
yusufnasserdev <yusufnassereng@gmail.com>
2023-02-17 05:43:23 +0200

    Modified phase1 notebook

--- Model/Phase 1/voice_actors.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-actors = pd.read_csv('datasets/movie-voice-actors.csv')
-actors.sort_values(['movie'], axis=0, inplace=True)
-
-actors.dropna(axis=0, how='any', thresh=None, subset=None, inplace=True)
-actors.to_csv('datasets/movie-voice-actors.csv')
